2c5608358e0d1cf72d6e35bcd8f1b590a5820cc3
memeda <readOnlyFile@hotmail.com>
2017-02-26 21:54:04 +0800

    copy R snake done.

--- R/script.R
+++ R/script.R
@@ -52,17 +52,18 @@ stage1 <- function() {
             keydown('q')
             return(TRUE)
             }
-        # head 碰到 tail
+        # head 碰到 tail( 头位置的颜色是尾巴的颜色，就说明头撞到尾巴了！)
         if (e$m[e$head[1], e$head[2]] == e$col_tail) 
         {
             print("game over: head hit tail")
-            keyword('q')
+            keydown('q')
             return(TRUE)
         }
         return(FALSE)
 
         }
     head <- function(){
+        # 先将头部的坐标保存起来
         e$lastx <- e$head[1]
         e$lasty <- e$head[2]
 
@@ -79,12 +80,16 @@ stage1 <- function() {
         e$m[e$head[1], e$head[2]] <- e$col_head
         # rbind  输入A、B，按行把B添加到A中！
         if(length(index(e$col_fruit)) <= 0){
-            e$tail <- rbind(e$tail, data.frame(x=e$lastx, y=e$lasty))
+            e$tail <- rbind(e$tail, data.frame(x=e$lastx, y=e$lasty)) 
         }
 
-        if(nrow(e$trail) > 0){
-            e$tail <- rbind(e$tail, data.fram(x=e$lastx, y=e$lasty))
+        # 下面的逻辑是： 如果有尾巴，那么就先把头(未移动的头，即 lastx, lasty) 加入到尾巴中，同时把尾巴中最后面的一个（最开始加入的那个）从尾巴中移除掉。
+        if(nrow(e$tail) > 0){
+
+            e$tail <- rbind(e$tail, data.frame(x=e$lastx, y=e$lasty))
+            # tail[1, ]表示取tail 的 第1行（R从1开始计数）， 而 tail[1]表示取第1列！ tail[1, 1]则表示取第一行、第一列
             e$m[e$tail[1,]$x, e$tail[1, ]$y] <- e$col_path
+            # 注意！！ tail[-1, ] 表示的是从tail中去掉第1行！！ （而非取倒数第一行！这个真是差别有点大...）
             e$tail <- e$tail[-1,]
             e$m[e$lastx, e$lasty] <- e$col_tail
         }
@@ -93,14 +98,93 @@ stage1 <- function() {
         print(paste("snake axis: ", e$head[1], e$head[2]))
     }
 
-
-
-
-
+    drawTable <- function(){
+        plot(0, 0, xlim=c(0, 1), ylim=c(0, 1), type='n', xaxs="i", yaxs="i")
     }
- }
+
+    drawMatrix <- function(){
+        # which 返回的是 矩阵的一维索引，这个一维索引是按列数的！就是说，先数第一列，输数第二列...
+        idx <- which(e$m > 0)
+        px <- (ifelse(idx%%e$width==0, e$width, idx%%e$width) - 1)/e$width + e$step / 2
+        py <- (ceiling(idx / e$height) - 1) / e$height + e$step / 2
+        pxy = data.frame(x=px, y=py, col=e$m[idx])
+        # points 是绘图函数！pch 是在坐标处绘制的符号， cex是膨胀率
+        points(pxy$x, pxy$y, col=pxy$col, pch=15, cex=4.4)
+    }
+
+    fruit()
+    head()
+    if(!fail()){
+        body()
+        drawTable()
+        drawMatrix()
+    }
+}
+
+stage0 <- function(){
+    e$stage <- 0
+    plot(0, 0, xlim=c(0, 1), ylim=c(0, 1), type='n', xaxs="i", yaxs="i")
+    text(0.5, 0.7, label="Sanke Game", cex=5)
+    text(0.5, 0.4, label="Any keyboard to start", cex=2, col=4)
+    text(0.5, 0.3, label="Up, Down, Left, Right to control direction", cex=2, col=2)
+    text(0.2, 0.05, label="Author: DanZhang", cex=1)
+    text(0.5, 0.05, label="http://blog.fens.me", cex=1)
+
+}
+
+stage2 <- function(){
+    e$stage <- 2
+    plot(0, 0, xlim=c(00, 1), ylim=c(0, 1), type='n', xaxs="i", yaxs="i")
+    text(0.5, 0.7, label="Game Over", cex=5)
+    text(0.5, 0.4, label="Space to restart, q to quit.", cex=2, col=4)
+    text(0.5, 0.3, label=paste("Congratulations! You have eat", nrow(e$tail), "fruits"))
+    text(0.2, 0.05, label="Author: DanZhang", cex=1)
+    text(0.5, 0.05, label="http://blog.fens.me", cex=1)
 
 
+}
+
+keydown <- function(K){
+    print(paste("keydown: ", K, "stage: ", e$stage))
+    if(e$stage == 0){
+        init()
+        stage1()
+        return(NULL)
+    }
+
+    if(e$stage==2){
+        if(K=='q'){
+            q()
+        }
+        else if(K == ' '){
+            stage0()
+        }
+        return(NULL)
+    }
+
+    if(e$stage==1){
+
+        if(K=="q"){
+            stage2()
+        }else{
+            if(tolower(K) %in% c("up", "down", "left", "right")){
+                e$lasted <- e$dir
+                e$dir <- tolower(K)
+                stage1()
+            }
+        }
+    }
+    return(NULL)
+}
+
+run <- function(){
+    par(mai=rep(0, 4), oma=rep(0, 4))
+    e<<- new.env()
+    stage0()
+
+    getGraphicsEvent(prompt="Snake Game", onKeybd=keydown)
 
 
+}
 
+run()
